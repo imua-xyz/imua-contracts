fc33dfadf3b756812a6b5638225acfa55da7a352
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2025-11-13 12:19:39 +0000

    test(CustomProxyAdmin): validate single use
    Add a test to check that upgrading from A to B to C is not permitted

--- test/foundry/unit/CustomProxyAdmin.t.sol
+++ test/foundry/unit/CustomProxyAdmin.t.sol
@@ -11,27 +11,27 @@ import "forge-std/Test.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
-contract StorageOld {
+/* ---- Storage contracts ---- */
+contract StorageA {
 
-    bool public implementationChanged;
-
-}
-
-contract StorageNew is StorageOld {
-
-    bool public hi;
+    uint256 public a;
 
 }
 
-contract ImplementationChanger is Initializable, StorageOld {
+contract StorageB is StorageA {
 
-    constructor() {
-        _disableInitializers();
-    }
+    uint256 public b;
 
-    function initialize() external initializer {
-        implementationChanged = false;
-    }
+}
+
+contract StorageC is StorageB {
+
+    uint256 public c;
+
+}
+
+/* ---- Base implementation contract ---- */
+abstract contract ImplementationChanger {
 
     function upgradeSelfToAndCall(address customProxyAdmin, address newImplementation) public {
         ICustomProxyAdmin(customProxyAdmin).upgradeSelfToAndCall(
@@ -39,103 +39,146 @@ contract ImplementationChanger is Initializable, StorageOld {
         );
     }
 
+    function initialize() external virtual;
+
 }
 
-contract NewImplementation is Initializable, StorageNew {
+/* ---- Implementation contracts ---- */
+// We use the same logic as Bootstrap and ClientChainGateway in the sense that
+// the downstream contracts inherit from their respective storage and not from the parent impl
+// We just use one common parent impl to avoid rewriting the code for the upgrade function.
+contract ImplementationA is Initializable, StorageA, ImplementationChanger {
 
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() external reinitializer(2) {
-        implementationChanged = true;
-        hi = true;
+    function initialize() external virtual override initializer {
+        a = 1;
+    }
+
+}
+
+contract ImplementationB is Initializable, StorageB, ImplementationChanger {
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() external virtual override reinitializer(2) {
+        a = 2;
+        b = 1;
+    }
+
+}
+
+contract ImplementationC is Initializable, StorageC, ImplementationChanger {
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() external virtual override reinitializer(3) {
+        a = 3;
+        b = 2;
+        c = 1;
     }
 
 }
 
 contract CustomProxyAdminTest is Test {
 
+    enum Stage {
+        A,
+        B,
+        C
+    }
+
     CustomProxyAdmin proxyAdmin;
+    ImplementationA implementationA;
+    address targetImpl;
 
     function setUp() public {
         proxyAdmin = new CustomProxyAdmin();
+        ImplementationA logic = new ImplementationA();
+        implementationA = ImplementationA(
+            address(
+                new TransparentUpgradeableProxy(
+                    address(logic), address(proxyAdmin), abi.encodeCall(ImplementationA.initialize, ())
+                )
+            )
+        );
+        // construct the new implementation for the tests
+        targetImpl = address(new ImplementationB());
+        // validate that the implementation has not changed
+        _validate(Stage.A);
+    }
+
+    function _initialize(address x) private {
+        proxyAdmin.initialize(x);
+    }
+
+    function _validate(Stage stage) private {
+        if (stage == Stage.A) {
+            assertTrue(implementationA.a() == 1);
+            vm.expectRevert();
+            uint256 b = ImplementationB(address(implementationA)).b();
+            vm.expectRevert();
+            uint256 c = ImplementationC(address(implementationA)).c();
+        } else if (stage == Stage.B) {
+            assertTrue(implementationA.a() == 2);
+            assertTrue(ImplementationB(address(implementationA)).b() == 1);
+            vm.expectRevert();
+            uint256 c = ImplementationC(address(implementationA)).c();
+        } else if (stage == Stage.C) {
+            assertTrue(implementationA.a() == 3);
+            assertTrue(ImplementationB(address(implementationA)).b() == 2);
+            assertTrue(ImplementationC(address(implementationA)).c() == 1);
+        } else {
+            revert("Invalid stage");
+        }
     }
 
     function test01_Initialize() public {
+        // check that 0 cannot be used
+        vm.expectRevert(abi.encodeWithSelector(Errors.ZeroAddress.selector));
+        _initialize(address(0x0));
+        // validate successful initialization for a new contract
         address proxy = address(0x123);
-        proxyAdmin.initialize(proxy);
+        _initialize(proxy);
         assertEq(proxyAdmin.proxy(), proxy);
-
-        vm.expectRevert();
+        // validate that it cannot be initialized again
+        vm.expectRevert("Initializable: contract is already initialized");
         proxyAdmin.initialize(address(0x1));
     }
 
     function test02_ChangeImplementation() public {
-        // initialize the contract
-        ImplementationChanger implementationChanger = ImplementationChanger(
-            address(
-                new TransparentUpgradeableProxy(
-                    address(new ImplementationChanger()),
-                    address(proxyAdmin),
-                    abi.encodeCall(ImplementationChanger.initialize, ())
-                )
-            )
-        );
-        // validate that the implementation has not changed already
-        assertFalse(implementationChanger.implementationChanged());
-        // check that it does not have a `hi` function in there.
-        NewImplementation newImplementation = NewImplementation(address(implementationChanger));
-        vm.expectRevert(); // EVM error
-        assertFalse(newImplementation.hi());
-        // now change the implementation
-        address targetImpl = address(new NewImplementation());
-        proxyAdmin.initialize(address(implementationChanger));
-        implementationChanger.upgradeSelfToAndCall(address(proxyAdmin), targetImpl);
-        // validate that it has changed
-        assertTrue(implementationChanger.implementationChanged());
-        assertTrue(newImplementation.hi());
+        _initialize(address(implementationA));
+        implementationA.upgradeSelfToAndCall(address(proxyAdmin), targetImpl);
+        _validate(Stage.B);
     }
 
     function test02_ChangeImplementation_NoProxySet() public {
-        // initialize the contract
-        ImplementationChanger implementationChanger = ImplementationChanger(
-            address(
-                new TransparentUpgradeableProxy(
-                    address(new ImplementationChanger()),
-                    address(proxyAdmin),
-                    abi.encodeCall(ImplementationChanger.initialize, ())
-                )
-            )
-        );
-        // validate that the implementation has not changed already
-        assertFalse(implementationChanger.implementationChanged());
-        address targetImpl = address(new NewImplementation());
         vm.expectRevert(abi.encodeWithSelector(Errors.CustomProxyAdminNoProxySet.selector));
-        implementationChanger.upgradeSelfToAndCall(address(proxyAdmin), targetImpl);
-        assertFalse(implementationChanger.implementationChanged());
+        implementationA.upgradeSelfToAndCall(address(proxyAdmin), targetImpl);
+        _validate(Stage.A);
     }
 
     function test02_ChangeImplementation_UnmanagedProxy() public {
-        // initialize the contract
-        ImplementationChanger implementationChanger = ImplementationChanger(
-            address(
-                new TransparentUpgradeableProxy(
-                    address(new ImplementationChanger()),
-                    address(proxyAdmin),
-                    abi.encodeCall(ImplementationChanger.initialize, ())
-                )
-            )
-        );
-        // set a random proxy
-        address randomProxy = address(0x123);
-        proxyAdmin.initialize(randomProxy);
-        // validate that the implementation has not changed already
-        assertFalse(implementationChanger.implementationChanged());
-        address targetImpl = address(new NewImplementation());
+        _initialize(address(0x1));
         vm.expectRevert(abi.encodeWithSelector(Errors.CustomProxyAdminUnmanagedProxy.selector));
-        implementationChanger.upgradeSelfToAndCall(address(proxyAdmin), targetImpl);
-        assertFalse(implementationChanger.implementationChanged());
+        implementationA.upgradeSelfToAndCall(address(proxyAdmin), targetImpl);
+        _validate(Stage.A);
+    }
+
+    function test03_ChangeImplementation_MultipleUpgrades() public {
+        test02_ChangeImplementation();
+        // now try to do a new upgrade
+        ImplementationC target = new ImplementationC();
+        vm.expectRevert();
+        implementationA.upgradeSelfToAndCall(address(proxyAdmin), address(target));
+        // validate that we are at stage B not C even though we tried to upgrade to C
+        _validate(Stage.B);
     }
 
 }
